426c2226e630a22c1ccadee4d95c8970cd2a3cac
Robert <Robert@10.13.49.7>
2024-11-12 12:49:25 +0200

    Clasa neon

---
+++ src/main/java/ro/cti/ugal/proiect/java/robert/Led.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ro.cti.ugal.proiect.java.robert;
+
+/**
+ *
+ * @author Robert
+ */
+public class Led {
+    public int culoareHex; //culoarea becului led
+    public Led(){
+        this.culoareHex = 0x000000ff; //culoarea becului led initiata in hexadecimal
+        
+        
+    }
+    
+    
+    
+}
