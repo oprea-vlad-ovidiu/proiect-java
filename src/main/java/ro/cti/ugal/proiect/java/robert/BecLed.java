4e28bb7de2b3384f962939b31a8213317dda4907
Robert <Robert@10.13.49.7>
2024-11-12 09:16:04 +0200

    Clasa bec cu led

---
+++ src/main/java/ro/cti/ugal/proiect/java/robert/BecLed.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ro.cti.ugal.proiect.java.robert;
+
+import ro.cti.ugal.proiect.java.SursaIluminat;
+
+/**
+ *
+ * @author Robert
+ */
+public class BecLed extends SursaIluminat {
+    
+    
+    
+}
