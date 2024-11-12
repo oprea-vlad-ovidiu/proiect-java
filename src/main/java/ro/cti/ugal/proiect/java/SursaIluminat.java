bf4ceccc141a7db445468a59cfc62ee1a14129de
oprea-vlad-ovidiu <opreavlad@protonmail.com>
2024-11-12 09:43:53 +0200

    Adaugat mai multe proprietati la SursaIluminat.

--- src/main/java/ro/cti/ugal/proiect/java/SursaIluminat.java
+++ src/main/java/ro/cti/ugal/proiect/java/SursaIluminat.java
@@ -1,11 +1,15 @@
 package ro.cti.ugal.proiect.java;
 
 public class SursaIluminat {
-    public int lightBrightness;     // In lumens
-    public int lightTemperature;    // In kelvin
+    public int lightBrightness;         // In lumens
+    public int lightTemperature;        // In kelvin
+    public int lightConsumption;        // In watts
+    public double lightColorAccuracy;   // Color Rendering Index
     
     public SursaIluminat() {
         this.lightBrightness = 0;
         this.lightTemperature = 0;
+        this.lightConsumption = 0;
+        this.lightColorAccuracy = 0.0;
     }
 }
