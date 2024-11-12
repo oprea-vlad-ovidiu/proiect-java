344f8d0b0f7dffda44824bef63a2926c7e5be3ba
oprea-vlad-ovidiu <opreavlad@protonmail.com>
2024-11-12 09:10:50 +0200

    Clasa comuna pentru SursaIluminat.

---
+++ src/main/java/ro/cti/ugal/proiect/java/SursaIluminat.java
@@ -0,0 +1,11 @@
+package ro.cti.ugal.proiect.java;
+
+public class SursaIluminat {
+    public int lightBrightness;     // In lumens
+    public int lightTemperature;    // In kelvin
+    
+    public SursaIluminat() {
+        this.lightBrightness = 0;
+        this.lightTemperature = 0;
+    }
+}
