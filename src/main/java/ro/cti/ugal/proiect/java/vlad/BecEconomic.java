975bbc7f324536502e0fd0920354867501ee2968
oprea-vlad-ovidiu <opreavlad@protonmail.com>
2024-11-18 17:17:13 +0200

    Adaugat datele membre la Bec Economic. Adaugat Bec Economic in test.

--- src/main/java/ro/cti/ugal/proiect/java/vlad/BecEconomic.java
+++ src/main/java/ro/cti/ugal/proiect/java/vlad/BecEconomic.java
@@ -3,5 +3,50 @@ package ro.cti.ugal.proiect.java.vlad;
 import ro.cti.ugal.proiect.java.SursaIluminat;
 
 public class BecEconomic extends SursaIluminat{
+    public double continutMercur;   // Toate becurile CFL contin mercur.
+    public double continutArgon;    // Toate becurile CFL contin argon.
+    public double durataIncalzire;  // Pasul 1 din pronirea unui bec este incalzirea catodului si anodului.
+    public String tipBalast;        // Magnetic(mai vechi) sau electric.
+    public boolean reglabil;        // Daca becul permite luminozitate reglabila.
     
+    public BecEconomic() {
+        super();
+        this.continutMercur = 0;
+        this.continutArgon = 0;
+        this.durataIncalzire = 0;
+        this.tipBalast = "n\\a";
+        this.reglabil = false;
+    }
+    
+    public BecEconomic(double continutMercur, double continutArgon, double durataIncalzire, 
+            String tipBalast, boolean reglabil, int luminozitate, int temperaturaCuloare, int consum, 
+            double indiceRedareCuloare) {
+        super(luminozitate, temperaturaCuloare, consum, indiceRedareCuloare);
+        
+        this.continutMercur = continutMercur;
+        this.continutArgon = continutArgon;
+        this.durataIncalzire = durataIncalzire;
+        this.tipBalast = tipBalast;
+        this.reglabil = reglabil;
+    }
+    
+    public BecEconomic(BecEconomic o) {
+        super(o);
+        
+        this.continutMercur = o.continutMercur;
+        this.continutArgon = o.continutArgon;
+        this.durataIncalzire = o.durataIncalzire;
+        this.tipBalast = o.tipBalast;
+        this.reglabil = o.reglabil;
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + 
+                "Continu Mercur: " + continutMercur + "mg\n" +
+                "Continut Argon: " + continutArgon + "mg\n" +
+                "Durata incalzire: " + durataIncalzire + "s\n" +
+                "Tip balast: " + tipBalast + "\n" +
+                "Reglabil: " + (reglabil ? "Da" : "Nu") + "\n";
+    }
 }
--- src/main/java/ro/cti/ugal/proiect/java/vlad/VladTest.java
+++ src/main/java/ro/cti/ugal/proiect/java/vlad/VladTest.java
@@ -2,14 +2,23 @@ package ro.cti.ugal.proiect.java.vlad;
 
 public class VladTest {
     public static void main(String[] args) {
-            BecClasic bec_gol = new BecClasic();
-            System.out.println(bec_gol);
+            BecClasic bec_clasic_gol = new BecClasic();
+            System.out.println(bec_clasic_gol);
             
             Material tungsten = new Material("Tungsten", 5.6e-8, 0.0045);
-            BecClasic bec = new BecClasic(tungsten, 0.58, 4.6e-5, 2700, 15);
-            System.out.println(bec);
+            BecClasic bec_clasic = new BecClasic(tungsten, 0.58, 4.6e-5, 2700, 15);
+            System.out.println(bec_clasic);
                
-            BecClasic copie = new BecClasic(bec);
-            System.out.println(copie);
+            BecClasic copie_clasic = new BecClasic(bec_clasic);
+            System.out.println(copie_clasic);
+            
+            BecEconomic bec_economic_gol = new BecEconomic();
+            System.out.println(bec_economic_gol);
+            
+            BecEconomic bec_economic = new BecEconomic(4, 1, 2.5, "Electronic", false, 1800, 4000, 26, 0.8);
+            System.out.println(bec_economic);
+            
+            BecEconomic copie_economic = new BecEconomic(bec_economic);
+            System.out.println(copie_economic);
     }
 }
