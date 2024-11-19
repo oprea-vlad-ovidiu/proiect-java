1deb19933c5d22c2d567346f2f74c679328227dc
Robert <Robert@10.13.49.7>
2024-11-19 08:58:38 +0200

    Parametrii neon

--- src/main/java/ro/cti/ugal/proiect/java/robert/Neon.java
+++ src/main/java/ro/cti/ugal/proiect/java/robert/Neon.java
@@ -13,15 +13,30 @@ import ro.cti.ugal.proiect.java.SursaIluminat;
 public class Neon extends SursaIluminat{
     
     public float gasVoltage;
+    public String gasType;
+    public int tubeDiameter, tubeLength;
+    public String tubeMaterial;
+    public boolean dimmable;
     
     public Neon()
     {
         this.gasVoltage = 0.0f;
+        this.gasType = "none";
+        this.tubeDiameter = 0;
+        this.tubeLength = 0;
+        this.tubeMaterial = "none";
+        this.dimmable = false;
+        
     }
     
-    public Neon(float gasVoltage)
+    public Neon(float gasVoltage, String gasType, int tubeD, int tubeL, String tubeMat, boolean dimmable)
     {
         this.gasVoltage = gasVoltage;
+        this.gasType = gasType;
+        this.tubeDiameter = tubeD;
+        this.tubeLength = tubeL;
+        this.tubeMaterial = tubeMat;
+        this.dimmable = dimmable;
     }
     
     public Neon(Neon other) {
--- src/main/java/ro/cti/ugal/proiect/java/robert/RobertTest.java
+++ src/main/java/ro/cti/ugal/proiect/java/robert/RobertTest.java
@@ -4,7 +4,7 @@ public class RobertTest {
     public static void main(String[] args) {
         BecLed b = new BecLed(2.5f, 10, 5, 6);
         System.out.println(b);
-        Neon n = new Neon(10.5f);
+        Neon n = new Neon(10.5f, "Argon & Mercury", 8, 30, "lead glass", true);
         System.out.println(n);
     }
 }
